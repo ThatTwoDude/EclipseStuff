93f770225e208d5058b5c65422c7a52e52b46353
ThatTwoDude <73597082+ThatTwoDude@users.noreply.github.com>
2022-02-24 16:14:40 -0700

    AHHHHHHHHH

--- eclipse-workspace/DemoProject/src/User.java
+++ eclipse-workspace/DemoProject/src/User.java
@@ -1,5 +1,6 @@
 //Java program to read data of various types using Scanner class.
 import java.util.Scanner;
+
 import java.sql;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,35 +8,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class User {
-	/*public static void main(String[] args) {
-		try (Connection conn = DriverManager.getConnection(
-				"jdbc:postgresql://127.0.0.1:5433/Inferno", "postgres", "postgres")) {
-			if (conn != null) {
-				System.out.println("Connected to the database!");
-				Statement st = conn.createStatement();
-				ResultSet rs = st.executeQuery("SELECT * FROM \"Users\" WHERE \"UserID\" >0");
-				while (rs.next())
-				{
-					System.out.print("Column 1 returned ");
-					System.out.println(rs.getString(2));
-				}
-				rs.close();
-				st.close();
-			} 
-			else {
-				System.out.println("Failed to make connection!");
-			}
 
-		} 
-		catch (SQLException e) {
-			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-		}	
-	}
-}*/
+public class User {
+
 	public static void main(String[] args) {
 		
 		try (Connection conn = DriverManager.getConnection(
@@ -55,6 +30,7 @@ public class User {
              st.close();
          
          
+         
          } else {
              System.out.println("Failed to make connection!");
          }
@@ -68,4 +44,27 @@ public class User {
 	}
 
 }
+/*
+
+import java.util.Vector;
+public interface DataManagere {
+	User.GetOne()
+}
+
+
+
+
+public class UserDataAdapter implements DataManager{
+
+}
+
+
+
+
+
+import java.sql.Connectioins;
+import java.sql
+*/
+
+
 
